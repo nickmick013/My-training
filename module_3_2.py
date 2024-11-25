ccae6b6b85b7e5e22139c7c0789ff964faaa00db
nickmick013 <nikita.sp0013@gmail.com>
2024-11-25 18:50:37 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -2,9 +2,9 @@
 def send_email(message, recipient, *, sender = "university.help@gmail.com"):
     if ('@' and '.com' and '.ru' and '.net') not in recipient and ('@' and '.com' and '.ru' and '.net') not in sender:
         print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
-    if recipient == sender:
+    elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
-    if sender == 'university.help@gmail.com':
+    elif sender:
         print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
     else:
         print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
