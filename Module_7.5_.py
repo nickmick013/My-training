5e3364a28e330a13d24850e3ced49b2c28a62dbb
nickmick013 <nikita.sp0013@gmail.com>
2025-01-14 15:47:53 +0300

    Add files via upload

--- Module_7.5_.py
+++ Module_7.5_.py
@@ -1,15 +1,25 @@
 import os
 import time
 
+from os.path import join, getmtime, getsize, dirname
+
 directory = '.'
 
 for root, dirs, files in os.walk(directory):
   for file in files:
-    filepath = os.path.join(file)
-    filetime = os.path.getmtime(file)
-    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-    file_size = os.path.getsize(file)
-    parent_dir = os.path.dirname(file)
-    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {file_size} байт,'
+
+
+    try:
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        file_size = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {file_size} байт,'
           f' Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+
+    except FileNotFoundError:
+        print(f'Файл {file} недоступен или был удален')
+
 
